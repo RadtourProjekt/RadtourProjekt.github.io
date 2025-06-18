94b7b6b96624a3ba452e8e42db006627b172586a
lujehle <lukas-jehle@web.de>
2025-06-18 11:34:24 +0200

    ordnerstruktur geändert

--- karte/main.js
+++ karte/main.js
@@ -1,26 +1,45 @@
+let vorarl = {
+    lat: 47.413635,
+    lng: 9.742376
+};
+
 let map = L.map("map", {
     maxZoom:19
-}).setView();
+}).setView([vorarl.lat, vorarl.lng],13);
 
-//Overlays definieren
-let overlays = {
-    Campingplätze: L.featureGroup().addTo(map),
-    Supermarkt: L.featureGroup().addTo(map),
-    Unterkünfte: L.featureGroup().addTo(map),
-    zones: L.featureGroup().addTo(map),
-
+// WMTS Hintergrundlayer der eGrundkarte Tirol
+let eGrundkarteTirol = {
+    sommer: L.tileLayer("https://wmts.kartetirol.at/gdi_summer/{z}/{x}/{y}.png", {
+        attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
+    }),
+    winter: L.tileLayer(
+        "https://wmts.kartetirol.at/gdi_winter/{z}/{x}/{y}.png", {
+        attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
+    }),
+    ortho: L.tileLayer("https://wmts.kartetirol.at/gdi_ortho/{z}/{x}/{y}.png", {
+        attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
+    }
+    ),
+    nomenklatur: L.tileLayer("https://wmts.kartetirol.at/gdi_nomenklatur/{z}/{x}/{y}.png", {
+        attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`,
+        pane: "overlayPane",
+    })
 }
 
-// Layercontrol
+// / Layer control mit eGrundkarte Tirol und Standardlayern
 L.control.layers({
-    "BasemapAT watercolour": L.tileLayer.provider('Stadia.StamenWatercolor').addTo(map),
-    "BasemapAT": L.tileLayer.provider('BasemapAT.basemap').addTo(map),
-    "BasemapAT surface": L.tileLayer.provider('BasemapAT.surface').addTo(map),
-    "BasemapAT orthophoto": L.tileLayer.provider('BasemapAT.orthofoto').addTo(map)
-}, {
-    "Sehenswürdigkeiten": overlays.sights,
-    "Vienna sightseeing Linien": overlays.lines,
-    "Vienna sightseeing Haltstellen": overlays.stops,
-    "Fußgängerzonen": overlays.zones,
-    "Hotels und Unterkünfte": overlays.hotels,
+    "eGrundkarte Tirol Sommer": L.layerGroup([
+        eGrundkarteTirol.sommer,
+        eGrundkarteTirol.nomenklatur
+    ]).addTo(map),
+    "eGrundkarte Tirol Winter": L.layerGroup([
+        eGrundkarteTirol.winter,
+        eGrundkarteTirol.nomenklatur
+    ]),
+    "eGrundkarte Tirol Orthofoto": L.layerGroup([
+        eGrundkarteTirol.ortho,
+        eGrundkarteTirol.nomenklatur,
+    ]),
+    "OpenStreetMap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
 }).addTo(map);
